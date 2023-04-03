f6d1d2c0dfa8b6f2279ddaf59131e2bae2688977
giseleme <126720208+giseleme@users.noreply.github.com>
2023-04-03 17:11:13 -0300

    oii

--- Dolentia Defense!/Assets/scripts/GameManager.cs
+++ Dolentia Defense!/Assets/scripts/GameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script1 : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,9 @@ public class script1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M))
+                { 
+                    SceneManager.LoadScene("MainMenu");
+                }
     }
-}
+
