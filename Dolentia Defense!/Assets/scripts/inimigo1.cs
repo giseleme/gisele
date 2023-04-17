4b80847265d11cbba6edc99c4834143aac3e575b
giseleme <126720208+giseleme@users.noreply.github.com>
2023-04-17 17:53:08 -0300

    boa noite

--- Dolentia Defense!/Assets/scripts/inimigo1.cs
+++ Dolentia Defense!/Assets/scripts/inimigo1.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class inimigo1 : MonoBehaviour
 {
-    float velocidade;
-  
+
     void Start()
     {
-        velocidade = 2f;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+        }
     }
 }
