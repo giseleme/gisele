1c51aaf28926135552a084dd33e399ceb9c65aa8
giseleme <gisele.campos@escolar.ifrn.edu.br>
2023-05-03 11:54:37 -0300

    infernooooooooooooooooo

--- Dolentia Defense!/Assets/scripts/inimigo1.cs
+++ Dolentia Defense!/Assets/scripts/inimigo1.cs
@@ -4,16 +4,49 @@ using UnityEngine;
 
 public class inimigo1 : MonoBehaviour
 {
+      public float speed = 10f; // velocidade de movimento do inimigo
+      public int health = 5; // quantidade de vida do inimigo
+    
+      private Rigidbody2D rb;
+      private bool isHit = false;
 
+    
+ 
     void Start()
     {
-        
+         rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
+    
+    
     void Update()
     {
+        if (!isHit)
+               {
+                   // Movimentação horizontal do inimigo pra esquerda
+                   Vector2 movement = new Vector2(-speed, 0);
+                   rb.MovePosition(rb.position + movement * Time.deltaTime);
+               }
        
-        }
+     }
+      void OnTriggerEnter2D(Collider2D other)
+         {
+               // Se o inimigo for atingido pela bala do jogador, perde 1 de vida
+                     if (other.tag == "Tirorapido")
+                     {
+                         health--;
+                         if (health <= 0)
+                         {
+                             Destroy(gameObject);
+                         }
+                         isHit = true;
+                         StartCoroutine(HitDelay());
+                     }
+         }
+     
+        IEnumerator HitDelay()
+        {
+             yield return new WaitForSeconds(0.2f); // aguarda 0.2 segundos
+             isHit = false;
+        }    
 }
 
